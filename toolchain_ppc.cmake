b9f4666b7daf2ddb963b1c470d741aa4ff333c16
Yury Rumyantsev <urock@fastsense.tech>
2020-07-09 09:47:34 +0300

    added toolchain_ppc.cmake file

---
+++ toolchain_ppc.cmake
@@ -0,0 +1,10 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+set(CMAKE_SYSTEM_PROCESSOR powerpc)
+
+set(tools /home/urock/work/modul/ppc/powerpc-module-linux-gnu)
+set(CMAKE_C_COMPILER ${tools}/bin/powerpc-module-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/powerpc-module-linux-gnu-g++) 
